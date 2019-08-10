d17a581414ca77a25804927838e01321070267ae
hardfist <1562502418@qq.com>
2019-08-10 23:55:51 +0800

    chore(spa): add test

---
+++ packages/spa/src/App.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { store } from './store';
+import App from './App';
+import { Provider } from 'react-redux';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Provider store={store as any}>
+      <App />
+    </Provider>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
