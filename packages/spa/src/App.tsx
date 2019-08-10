ec4d0ca9c4eb5cd0d8d1f1e6b76dad8f8cf6e599
yangjian.fe <yangjian.fe@bytedance.com>
2019-08-10 22:31:33 +0800

    refactor(spa): 使用styled-components 重构

--- packages/spa/src/App.tsx
+++ packages/spa/src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from 'styled-components';
+import { GlobalStyle } from './global';
+import { ReactComponent as LogoIcon } from './logo.svg';
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+const AppHeader = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+const app_logo_spin = keyframes`
+from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+const Logo = styled(LogoIcon)`
+  animation: ${app_logo_spin} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+const Link = styled.a`
+  color: #61dafb;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <AppWrapper>
+        <AppHeader>
+          <Logo />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <Link
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </Link>
+        </AppHeader>
+      </AppWrapper>
+    </>
   );
-}
+};
 
 export default App;
--- packages/spa/src/global.tsx
+++ packages/spa/src/global.tsx
@@ -1,3 +1,5 @@
+import { createGlobalStyle } from 'styled-components';
+export const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
@@ -6,8 +8,8 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-
 code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+`;
