55e612500fe769dc72a6a297ce713a75d2439212
hardfist <1562502418@qq.com>
2019-08-13 00:48:18 +0800

    feat(spa): add theme support

--- packages/spa/src/components/layout.tsx
+++ packages/spa/src/components/layout.tsx
@@ -6,7 +6,7 @@ import { Header } from 'components/header';
 const LayoutWrapper = styled.div`
   position: relative;
   padding: 42px 21px;
-  background-color: #282c34;
+  background-color: ${p => p.theme.bg};
 `;
 export const Layout: React.FC = props => {
   return (
