c5c3a7b029bee1012513f3d8f8cac9cc5bb026fd
hardfist <1562502418@qq.com>
2019-08-13 14:18:59 +0800

    feat(spa): 动态导入使用@loadable/component

--- packages/spa/src/pages/detail.tsx
+++ packages/spa/src/pages/detail.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 export const Detail = () => {
-  return <div>detail</div>;
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } });
+  return <animated.div style={props}>I will fade in</animated.div>;
 };
--- packages/spa/src/routes.tsx
+++ packages/spa/src/routes.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import loadable from '@loadable/component';
 import { wrapDefault } from 'utils/export';
+const Detail = loadable(wrapDefault(() => import('pages/detail'), 'Detail'));
+const Home = loadable(wrapDefault(() => import('pages/home'), 'Home'));
 export const routes = [
   {
     path: '/',
     exact: true,
-    component: React.lazy(wrapDefault(() => import('pages/home'), 'Home'))
+    component: Home
+  },
+  {
+    path: '/detail',
+    exact: true,
+    component: Detail
   }
 ];
