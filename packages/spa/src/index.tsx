29be294b2e97737b2a192701f8494ea16f489851
hardfist <1562502418@qq.com>
2019-08-18 09:14:44 +0800

    feat(spa): change browserrouter to hashrouter

--- packages/spa/src/index.tsx
+++ packages/spa/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
@@ -10,9 +10,9 @@ typography.injectStyles();
 
 ReactDOM.render(
   <Provider store={store as any}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
