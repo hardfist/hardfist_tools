3320f36be6a07b6c3f4d30ee6c92933f230ee851
hardfist <1562502418@qq.com>
2019-09-21 16:18:40 +0800

    feat(spa): 重构

---
+++ packages/spa/src/pages/redux/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ReduxCounter } from 'components/counter';
+
+export const Redux = () => {
+  return <ReduxCounter />;
+};
---
+++ packages/spa/src/pages/todo_list/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { TodoList as Todo } from 'components/todo_list';
+
+export const TodoList = () => {
+  return <Todo />;
+};
