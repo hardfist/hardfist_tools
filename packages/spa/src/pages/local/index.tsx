cdb81d1a3e22b78125760885ff80b41f2826a289
hardfist <1562502418@qq.com>
2019-09-21 14:34:33 +0800

    feat(spa): add local state

---
+++ packages/spa/src/pages/local/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Counter } from 'components/counter';
+
+export const Local = () => {
+  return (
+    <>
+      <Counter />
+      <Counter />
+    </>
+  );
+};
