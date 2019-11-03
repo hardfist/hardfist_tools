5c1b7fdc89deb32b6a258d743093264ffddde864
hardfist <1562502418@qq.com>
2019-11-03 13:35:44 +0800

    feat(helpers): add typescript support

--- packages/helpers/rollup.config.js
+++ packages/helpers/rollup.config.js
@@ -1,14 +1,17 @@
 import resolve from 'rollup-plugin-node-resolve';
 import visualizer from 'rollup-plugin-visualizer';
+import path from 'path';
 import sizes from 'rollup-plugin-sizes';
 import filesize from 'rollup-plugin-filesize';
 import analyze from 'rollup-plugin-analyzer';
+import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import sourcemap from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 // eslint-disable-next-line import/no-default-export
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: [
     {
       file: 'lib/secret.js',
@@ -22,8 +25,15 @@ export default {
   ],
   external: ['lodash-es', 'lodash-es/map', 'the-answer'],
   plugins: [
+    commonjs(),
     resolve(),
     json(),
+    typescript({
+      check: process.env.NODE_ENV === 'production',
+      tsconfig: path.resolve('tsconfig.json'),
+      tsconfigOverride: {},
+      objectHashIgnoreUnknownHack: true,
+    }),
     sourcemap(),
     visualizer(),
     sizes(),
--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -1,8 +1,9 @@
 import answer from 'the-answer';
 import find from 'lodash-es/find';
 import { version } from '../package.json';
-// import map from 'lodash-es/map';
-export function myanswer() {
+// import { connect } from 'react-redux';
+export async function myanswer(str: string) {
   console.log('the answer is ' + answer + find('hello world', 'world'));
   console.log('version:', version);
+  // console.log('connect:', connect);
 }
