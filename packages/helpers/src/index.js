8d67cc38e032b89b6c57aa8b7993f97c7df0f721
hardfist <1562502418@qq.com>
2019-11-03 12:10:02 +0800

    feat(helpers): add sizes

--- packages/helpers/src/index.js
+++ packages/helpers/src/index.js
@@ -1,6 +1,6 @@
 import answer from 'the-answer';
-import * as _ from 'lodash';
+import find from 'lodash-es/find';
 // import map from 'lodash-es/map';
 export function myanswer() {
-  console.log('the answer is ' + answer + _.find('hello world', 'world'));
+  console.log('the answer is ' + answer + find('hello world', 'world'));
 }
