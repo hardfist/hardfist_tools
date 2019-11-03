1c849298c32452888f694373fc65327cfa36879f
hardfist <1562502418@qq.com>
2019-11-03 12:06:49 +0800

    feat(helpers): add visualizer

--- packages/helpers/src/index.js
+++ packages/helpers/src/index.js
@@ -1,5 +1,6 @@
 import answer from 'the-answer';
+import * as _ from 'lodash';
 // import map from 'lodash-es/map';
 export function myanswer() {
-  console.log('the answer is ' + answer);
+  console.log('the answer is ' + answer + _.find('hello world', 'world'));
 }
